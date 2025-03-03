31414917d7d1c869ed8dd9b5cdca5b6127ad81b0
panupong_jar <panupong.jar@ku.th>
2025-03-03 13:23:07 +0700

    update

--- server/index.js
+++ server/index.js
@@ -1,105 +1,111 @@
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
-const mysql = require('mysql2/promise')
+const mysql = require('mysql2/promise');
+const cors = require('cors');
+const app = express();
 
 const port = 8000;
-
 app.use(bodyParser.json());
+app.use(cors());
 
-let users = [];
-let counter = 1;
+let users = []
 
 let conn = null
-
 const initMySQL = async () => {
-    const conn = await mysql.createConnection({
+    conn = await mysql.createConnection({
         host: 'localhost',
-            user: 'root',
-            database: 'webdb',
-            port: 8850
-        })
+        user: 'root',
+        password: 'root',
+        database: 'webdb',
+        port: 8820
+    })
 }
 
-//path: GET /users สำหรับ get users ทั้งหมดที่บันทึกไว้
-app.get('/users',async (req, res) => {
-    const result =  await conn.query('SELECT * FROM users')
-    res.json(result[0])
+// path = GET /users สำหรับ get users ทั้งหมดที่บันทึกไว้
+app.get('/users', async (req, res) => {
+    const results = await conn.query('SELECT * FROM users')
+    res.json(results[0])
 })
 
-//path: POST /user สำหรับสร้าง user ใหม่บันทึกเข้าไป
-app.post('/users',async (req, res) => {
-    let user = req.body;
-    const result = await conn.query('INSERT INTO users SET ?', user)
-    console.log('result', result)
-    res.json({
-        message: 'Create user successfully',
-        data: result[0]
+// path = POST /users สำหรับสร้าง users ใหม่บันทึกเข้าไป
+app.post('/users', async (req, res) => {
+
+    try {
+        let user = req.body;
+        const results = await conn.query('INSERT INTO users SET ?', user) //คำสั่ง SQL สำหรับเพิ่มข้อมูลลงในตาราง users
+        res.json({
+            message: 'Create user successfully',
+            data: results[0]
+        })
+    } catch (error) {
+        console.log("error:", error.message)
+        res.status(500).json({
+            message: 'something went wrong',
+            errorMessage: error.message
+        })
+    }
 })
-})
 
-//path: GET /users/:id สำหรับดึง  users รายคนออกมา
-app.get('/users/:id', (req, res) => {
-    let id = req.params.id;
-    //ค้นหา users จาก id ที่ส่งมา
-    let selectedIndex = users.findIndex(user => user.id == id)
-
-    res.json(users[selectedIndex])
-})
-
-
-//path: PUT /user/:id ใช้สำหรับแก้ไขข้อมูล user ที่มี id ตามที่ระบุ
-app.put('/users/:id', (req, res) => {
-    let id = req.params.id;
-    let updateUser = req.body;
-    let selectedIndex = users.findIndex(user => user.id == id)
-
-    users[selectedIndex].firstname = updateUser.firstname || users[selectedIndex].firstname
-    users[selectedIndex].lastname = updateUser.lastname || users[selectedIndex].lastname
-    users[selectedIndex].age = updateUser.age || users[selectedIndex].age
-    users[selectedIndex].grnder = updateUser.gender || users[selectedIndex].gender
-
-    res.json({
-        message: 'Update user successfully',
-        data: {
-            user: updateUser,
-            indexUpdate: selectedIndex
+// path = GET /users/:id สำหรับดึง users รายคนออกมา
+app.get('/users/:id', async (req, res) => {
+    try {
+        let id = req.params.id;
+        const results = await conn.query('SELECT * FROM users WHERE id = ?', id)
+        if (results[0].length == 0) {
+            throw { statusCode: 404, message: "User not found" }
         }
-    })
+        res.json(results[0][0])
+    } catch (error) {
+        console.log("error:", error.message)
+        let statusCode = error.statusCode || 500
+        res.status(500).json({
+            message: 'something went wrong',
+            errorMessage: error.message
+        })
+    }
 })
 
-//path: DELETE /user/:id ใช้สำหรับลบข้อมูล user ที่มี id ตามที่ระบุ
-app.delete('/users/:id', (req, res) => {
-    let id = req.params.id;
-    //หาusers จาก user ที่ต้องการลบ
-    let selectedIndex = users.findIndex(user => user.id == id)
 
-    //ลบข้อมูล user
-    users.splice(selectedIndex, 1)
-    res.json({
-        message: 'Delete user successfully',
-        indexDelete: selectedIndex
-    })
+//path: PUT /users/:id สำหรับแก้ไข users รายคน (ตาม id ที่บันทึกเข้าไป)
+app.put('/users/:id', async (req, res) => {
+    try {
+        let id = req.params.id;
+        let updateUser = req.body;
+        const results = await conn.query(
+            'UPDATE users SET ? WHERE id = ?',
+            [updateUser, id]
+        )
+        res.json({
+            message: 'Create user successfully',
+            data: results[0]
+        })
+    } catch (error) {
+        console.log("error:", error.message)
+        res.status(500).json({
+            message: 'something went wrong',
+            errorMessage: error.message
+        })
+    }
 })
 
-app.listen(port, (req, res) => {
-    console.log('Http Server is running on port ' + port);
+//path: DELETE /users/:id สำหรับลบ users รายคน (ตาม id ที่บันทึกเข้าไป)
+app.delete('/users/:id', async (req, res) => {
+    try {
+        let id = req.params.id;
+        const results = await conn.query('DELETE from users WHERE id = ?', parseInt(id))
+        res.json({
+            message: 'Delete user successfully',
+            data: results[0]
+        })
+    } catch (error) {
+        console.log("error:", error.message)
+        res.status(500).json({
+            message: 'something went wrong',
+            errorMessage: error.message
+        })
+    }
 });
-
-//path: GET /users/:id สำหรับดึง  users รายคนออกมา
-app.get('/users', (req, res) => {
-    let id = req.params.id;
-    const filterUsers = users.map(user => {
-        return {
-            firstname: user.firstname,
-            lastname: user.lastname,
-            fullname: user.firstname + ' ' + user.lastname
-        }
-    })
-    res.json(filterUsers)
-})
-
-app.listen(port,async (req, res) => {
+app.listen(port, async (req, res) => {
     await initMySQL()
-    console.log('Http Server is running on port ' + port);
-})
+    console.log("Http Server is running on port" + port)
+});
